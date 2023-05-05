bcf2acef208ef51cc760d26447a144eececbd838
LuizWolf12 <65361452+GabrielWolf-Dev@users.noreply.github.com>
2023-05-05 18:08:59 -0300

    feat: add packages and first connection db

---
+++ db/conn.js
@@ -0,0 +1,16 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("toughts", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+  port: "3308",
+});
+
+try {
+  sequelize.authenticate();
+  console.log("Banco conectado com sucesso!");
+} catch (error) {
+  console.error(`Não foi possível conectar: ${error}`);
+}
+
+module.exports = sequelize;
